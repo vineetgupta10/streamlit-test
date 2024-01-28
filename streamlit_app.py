1003ce8b3c823ea56c5ada13280d38ea9b8fb862
Vineet Gupta <vineetgupta10@gmail.com>
2024-01-28 19:07:11 +0000

    Another small commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,23 @@
 import streamlit as st
 from hugchat import hugchat
-from hugchat.login import login
+from hugchat.login import Login
+
+st.set_page_config(page_title="🤗💬 HugChat")
 
 st.title('🎈 App Name 2')
+
+with st.sidebar:
+    st.title('🤗💬 HugChat')
+    if ('EMAIL' in st.secrets) and ('PASS' in st.secrets):
+        st.success('HuggingFace Login credentials already provided!', icon='✅')
+        hf_email = st.secrets['EMAIL']
+        hf_pass = st.secrets['PASS']
+    else:
+        hf_email = st.text_input('Enter E-mail:', type='password')
+        hf_pass = st.text_input('Enter password:', type='password')
+        if not (hf_email and hf_pass):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
 
 st.write('Hello world!')
